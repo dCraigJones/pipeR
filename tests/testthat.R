095eeb8e4f407f622a88e39d3c3d883c172f9904
Craig Jones <jones.dcraig@gmail.com>
2020-04-08 00:09:06 -0400

    initalize testing

---
+++ tests/testthat.R
@@ -0,0 +1,12 @@
+library(testthat)
+library(pipeR)
+
+test_check("pipeR")
+
+context("hydraulics")
+
+test_that("mannings_properties_are_correct", {
+  expect_equal(round(calc_hyd_radius(8,8),4), 0.1667)
+  expect_equal(round(calc_hyd_radius(2,8),4), 0.0978)
+  expect_equal(round(calc_hyd_radius(4,8),4), 0.1667)
+})
